1e72a489c55462eb3e21f0e96ae7f74237151bf9
jisoo kim <jisookim20032003@gmail.com>
2020-08-12 17:28:23 -0700

    remove

--- textme/sms.py
+++ textme/sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client 
  
-account_sid = 'AC66bfca55c37aaf76cd4566d89a2c51a3' 
-auth_token = '7e38557e30563e8383e0cdee5bc962d9' 
+account_sid = 
+auth_token = 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create( 
                               from_='+12253145542', 
-                              body ='Ligma balls',       
-                              to='+15622298999' 
+                              body ='',       
+                              to='' 
                           ) 
  
 print(message.sid)
